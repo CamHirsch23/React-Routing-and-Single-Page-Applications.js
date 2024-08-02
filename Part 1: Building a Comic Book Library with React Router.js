bdf0d443ac92222426fe20156225c90c0ade3cd2
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-02 14:55:10 -0500

    Create Part 1: Building a Comic Book Library with React Router.js

---
+++ Part 1: Building a Comic Book Library with React Router.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+``` [[8]](https://code.visualstudio.com/docs/nodejs/reactjs-tutorial#:~:text=React%20is%20a%20popular%20JavaScript%20library%20developed%20by%20Facebook%20for%20building%20user%20interfaces)[[9]](https://www.youtube.com/watch?v=nDGA3km5He4#:~:text=4%20introduced%20a%20whole%20new%20set%20of%20tools%20that%20you%20can%20use%20in%20react%20router%20I%27m%20planning%20on%20touching%20on%20those%20new%20additions%20to%206)[[3]](https://www.instagram.com/coding__comics/p/C8JHxeQhlIY/?locale=es_ES#:~:text=Coding%20Comics%20%F0%9F%8E%AF%20Web%20Development%20%26%20Designing%20%F0%9F%92%A3%20React%20js%20%7C%20React%20Router%20is%20a%20standard%20library%20for%20routing%20in%20React)[[4]](https://luqmanshaban.medium.com/react-router-a-step-by-step-guide-4c5ec964d2e9#:~:text=React%20Router%20is%20a%20crucial%20tool%20for%20building%20dynamic%2C%20single%2Dpage%20applications%20in%20React).
