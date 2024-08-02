f0c9acd6ad30dbaeb499f4a3f225f150f57379b1
CamHirsch23 <hirschboeck13@gmail.com>
2024-08-02 14:55:56 -0500

    Create Create Route Components.js

---
+++ Create Route Components.js
@@ -0,0 +1,12 @@
+// src/components/Comics.jsx
+const Comics = () => {
+    return (
+        <div>
+            <h2>Comics</h2>
+            <p>This component is under construction. Check back later!</p>
+        </div>
+    );
+};
+
+export default Comics;
+``` [[10]](https://code.visualstudio.com/docs/nodejs/reactjs-tutorial#:~:text=js%20IntelliSense%20and%20code%20navigation%20out%20of%20the%20box)[[9]](https://www.youtube.com/watch?v=nDGA3km5He4#:~:text=4%20introduced%20a%20whole%20new%20set%20of%20tools%20that%20you%20can%20use%20in%20react%20router%20I%27m%20planning%20on%20touching%20on%20those%20new%20additions%20to%206)[[11]](https://www.instagram.com/coding__comics/p/C8JHxeQhlIY/?locale=es_ES#:~:text=%2A%2ACreate%20Route%20Components%2A%2A)[[12]](https://luqmanshaban.medium.com/react-router-a-step-by-step-guide-4c5ec964d2e9#:~:text=Route).
